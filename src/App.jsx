03ce85abb716b110b34c336d39ecb8d5b955bed9
k-natsuyama <100003813+k-natsuyama@users.noreply.github.com>
2022-03-24 00:38:30 +0900

    1st

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  return (
+    <React.Fragment>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </React.Fragment>
+  );
+};
+
+export default App;
